657ce576a75b84011f2c67f5b53d7566d0c976db
swag-master0 <22204@otagogirls.school.nz>
2024-08-10 14:34:50 +1200

    rizz meter alive again!!

--- Assets/Scripts/rizzmeter.cs
+++ Assets/Scripts/rizzmeter.cs
@@ -6,25 +6,24 @@ using UnityEngine.UI;
 public class rizzmeter : MonoBehaviour
 {
     public int rizz;
-    public Transform meterpercentage;
     public Transform colour;
     public int maxrizz = 100;
     public int minrizz = 0;
-    public Color32 positivecolor;
-    public Color32 negativecolor;
-    Image rizzimg;
+    public Image rizzimg;
+    public Color startColor;
+    public Color endColor;
+
     // Start is called before the first frame update
     void Start()
     {
-        rizzimg = colour.GetComponent<Image>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //rizz = rizz + Time.deltaTime/5;
-        Debug.Log($"rizz is {rizz}");
-        meterpercentage.localScale = new Vector3(1,rizz/(float)maxrizz,1);
+        rizzimg.fillAmount = rizz / (float)maxrizz;
+        Debug.Log($"fill amount is {rizzimg.fillAmount}");
         if (rizz > maxrizz)
         {
             rizz = maxrizz;
@@ -33,13 +32,6 @@ public class rizzmeter : MonoBehaviour
         {
             rizz = minrizz;
         }
-        if (rizz < 0)
-        {
-            rizzimg.color = negativecolor;
-        }
-        else
-        {
-            rizzimg.color = positivecolor;
-        }
+        rizzimg.color = Color.Lerp(startColor, endColor, rizzimg.fillAmount);
     }
 }
