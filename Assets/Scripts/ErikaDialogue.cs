d20430d7c27f313f560a3aac323f5e952e649935
Erika Stuart (1000106562) <erikat.stuart@gmail.com>
2024-08-11 14:16:00 +1200

    Dialogue function

---
+++ Assets/Scripts/ErikaDialogue.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[Serializable]
+public struct DialogueOptions
+{
+    public string bachelorText;
+
+    public string response1;
+    public string response2;
+    public string response3;
+}
+
+public class ErikaDialogue : MonoBehaviour
+{
+    public List<DialogueOptions> dialogueOptions;
+    public TextMeshProUGUI bachelorTextObj;
+    [SerializeField] private TextMeshProUGUI option1;
+    [SerializeField] private TextMeshProUGUI option2;
+    [SerializeField] private TextMeshProUGUI option3;
+    private int index = 0;
+    public GameObject Responses1; //Parent objects of the buttons
+
+
+
+    public static ErikaDialogue Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        bachelorTextObj.text = dialogueOptions[index].bachelorText;
+    }
+
+    public void IterateConvo()
+    {
+        if (index < dialogueOptions.Count - 1)
+        {
+            index++;
+            UpdateUI();
+        }
+        else
+        {
+            Debug.Log("END --> CHECK RIZZ METER LEVEL");
+        }
+    }
+
+    public IEnumerator ReactionPause()
+    {
+        yield return new WaitForSeconds(2);
+        IterateConvo();
+        Responses1.SetActive(false);
+
+    }
+    
+}
--- Assets/scripts/DialogueButtons.cs
+++ Assets/scripts/DialogueButtons.cs
@@ -1,84 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueButtons : MonoBehaviour
 {
     public int rizzMeter;
-    public GameObject Responses1; //Parent objects of the buttons
+    [SerializeField]private List<Sprite> reactions;
+    public Image reactionImage;
 
-
-    public GameObject Responses2;
-
-
-    public GameObject Responses3;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void Response() //When button 1 is clicked
     {
-        Debug.Log("this is for response 1"); //Console check thing
         rizzMeter += 10; //makes the rizz meter increase depending on what option users pick
-        Responses1.SetActive(false); //makes the previous buttons disappear after users pick a option - takes them to new dialogue options
+        reactionImage.sprite = reactions[0];
+
+        ErikaDialogue.Instance.bachelorTextObj.text = "Thats cool.";
+        StartCoroutine(ErikaDialogue.Instance.ReactionPause());
+
     }
         public void Response2() //When button 2 is clicked
     {
-        Debug.Log("this is for response 2");
         rizzMeter += 20;
-        Responses1.SetActive(false);
+        reactionImage.sprite = reactions[1];
+
+        ErikaDialogue.Instance.bachelorTextObj.text = "SUGOI!";
+        StartCoroutine(ErikaDialogue.Instance.ReactionPause());
     }
         public void Response3()
     {
-        Debug.Log("this is for response 3");
         rizzMeter -= 10;
-        Responses1.SetActive(false);
+        reactionImage.sprite = reactions[2];
+
+        ErikaDialogue.Instance.bachelorTextObj.text = "Um... Ok?";
+        StartCoroutine(ErikaDialogue.Instance.ReactionPause());
     }
 
-
-        public void Oay()
+    public void Continue()
     {
-        Debug.Log("this is for response 1");
-        rizzMeter += 10;
-        Responses2.SetActive(false);
+        ErikaDialogue.Instance.Responses1.SetActive(true);
     }
-        public void Oay2()
-    {
-        Debug.Log("this is for response 2");
-        rizzMeter += 20;
-         Responses2.SetActive(false);
-    }
-        public void Oay3()
-    {
-        Debug.Log("this is for response 3");
-        rizzMeter -= 10;
-         Responses2.SetActive(false);
-    }
-
-
-    public void Beans()
-    {
-        Debug.Log("this is for response 3");
-        rizzMeter += 10;
-        Responses3.SetActive(false);
-    }
-     public void Beans2()
-    {
-        Debug.Log("this is for response 3");
-        rizzMeter += 20;
-        Responses3.SetActive(false);
-    }
-    public void Beans3()
-    {
-        Debug.Log("this is for response 3");
-        rizzMeter -= 10;
-        Responses3.SetActive(false);
-    }
+     
 }
